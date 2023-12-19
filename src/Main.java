7b4fb155df3458c575d3b45c805b6fb354a81353
Andrei <mirceanghel98@gmail.com>
2023-12-19 20:48:37 +0200

    LeetCode resolve

--- src/Main.java
+++ src/Main.java
@@ -3,43 +3,51 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> songs = Arrays.asList(30, 30, 50, 10, 70, 50, 20, 40, 50, 10);
-//        List<Integer> songs = Arrays.asList(30, 30, 50, 10, 70);
+        int[] time = {70,311,74,427,232,211,126,83,357,118,97,428};
+        System.out.println(numPairsDivisibleBy60(time));
+    }
+
+
+    private static int numPairsDivisibleBy60(int[] time) {
+        //List<Integer> songs = Arrays.asList(30, 30, 50, 10, 70, 50, 20, 40, 50, 10, 50, 10);
+        //List<Integer> songs = Arrays.asList(time);
+        // List<Integer> songs = Arrays.stream(time).boxed().toList();
 
         int current = 0;
         int count = 0;
+        Arrays.sort(time);
 
-        List<Integer> songsHelp = new ArrayList(songs);
-        Collections.sort(songsHelp);
-        System.out.println("Sorted list: " + songsHelp + "\n");
+        // List<Integer> songsHelp = new ArrayList(songs);
+        // Collections.sort(songsHelp);
+        // System.out.println("Sorted list: " + songsHelp + "\n");
 
-        int index = songsHelp.size() - 1;
+        int index = time.length - 1;
 
-        while (index >= 0) {
-            if (songsHelp.size() == 1 || songsHelp.isEmpty()) {
-                break;
-            }
+        while (index >= current) {
+            // if (songsHelp.size() == 1 || songsHelp.isEmpty()) {
+            //     break;
+            // }
 
-            if (index > (songsHelp.size() - 1)) {
+            // if (index > (songsHelp.size() - 1)) {
+            //     index--;
+            // } else {
+            if ((time[current] + time[index]) % 60 == 0) {
+                count++;
+                // songsHelp.remove(index);
+                // songsHelp.remove(current);
+                // current += 1;
                 index--;
             } else {
-                if ((songsHelp.get(current) + songsHelp.get(index)) % 60 == 0) {
-                    count++;
-                    songsHelp.remove(index);
-                    songsHelp.remove(current);
-                    current = 0;
-                    index = songsHelp.size() - 1;
-                } else {
-                    index--;
-                }
+                index--;
+            }
 
-                if (index == current + 1) {
-                    current++;
-                }
+            if (index == current + 1) {
+                current++;
+                index = time.length - 1;
             }
         }
+        // }
 
-        System.out.println("Initial list: " + songs);
-        System.out.println("Pair count: " + count);
+        return count;
     }
 }
